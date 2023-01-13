ae41ac21e8226a27183f758752964551b915cf57
peeturasanen <peetu.rasanen@metropolia.fi>
2023-01-13 14:51:28 +0200

    Teht2

---
+++ Moduuli02/Teht4.py
@@ -0,0 +1,11 @@
+luku1 = int(input('Anna kokonaisluku: '))
+luku2 = int(input('Anna toinen kokonaisluku: '))
+luku3 = int(input('Anna kolmas kokonaisluku: '))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1*luku2*luku3
+keskiarvo = tulo / 3
+
+print('Lukujen summa: ' +str(summa))
+print('Lukujen tulo: ' +str(tulo))
+print('Lukujen keskiarvo: ' +str(keskiarvo))
